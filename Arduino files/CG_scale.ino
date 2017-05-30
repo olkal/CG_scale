65a0374ce65a2e09c67c29a37f0c4eaec216a065
Olav Kallhovd <olav.kallhovd@gmail.com>
2017-05-30 21:09:32 +0200

    Update CG_scale.ino
    Simplifying code + fixed some bugs

--- Arduino files/CG_scale.ino
+++ Arduino files/CG_scale.ino
@@ -1,5 +1,5 @@
 /*
- * CG scale for F3F/F3B models
+ * CG scale for F3F & F3B models
  * Olav Kallhovd 2016
  * 
    CG Scale main components:
@@ -9,27 +9,16 @@
    1 pc Arduino Pro
    1 pc Serial display (16*2 LCD w/ Arduino Pro)
 
-   Max loading with suggested sensors: 4 to 4,5kg depending on CG location
+   Max model weight with suggested sensors: 4 to 4,5kg depending on CG location
 */
 
 #include "HX711.h" //https://github.com/bogde/HX711
-#include "EEPROM.h"
 
-#define EEPROM_SENS1_ADR 2
-#define EEPROM_SENS2_ADR 6
-#define EEPROM_PEGDIST_ADR 10
-#define EEPROM_STOPDIST_ADR 14
-#define EEPROM_BATFACT_ADR 18
+HX711 scale_1(A2, A3);//HX711 pins front sensor (DOUT, PD_SCK)
+HX711 scale_2(A0, A1);//HX711 pins rear sensor (DOUT, PD_SCK)
 
-HX711 scale_1(A0, A1);// HX711 pins ver A01
-HX711 scale_2(A2, A3);//HX711 pins ver A01
-byte ledPin = 3;//ver A01
-byte batRefPin = A4; // ver A01
-
-//HX711 scale_1(6, 7);//HX711 pins ver B01
-//HX711 scale_2(3, 4);//HX711 pins ver B01
-//byte ledPin = 9;//ver B01
-//byte batRefPin = A3;//ver B01
+byte ledPin = 3;
+byte batRefPin = A4;
 
 long battValue;
 long weightAvr[3];
@@ -37,59 +26,27 @@ long weightSng[3];
 long weightTot;
 float CGratio;
 long CG;
-long WingPegDist; //mm *10
-long LEstopperDist; //mm *10
-long CGoffset;
+long WingPegDist = 1198; //calibration value in 1/10mm, projected distance between wing support points, measure with calliper
+long LEstopperDist = 300; //calibration value 1/10mm, projected distance from front wing support point to leading edge (stopper pin), measure with calliper
+long CGoffset = ((WingPegDist / 2) + LEstopperDist) * 10;;
 char toLCD[20];
 boolean output;
-
 long t;
 
 void setup() {
   Serial.begin(9600);
-  //calibration settings, write to EEPROM for new setup:
-  /*
-    //A01 1st prototype:
-    int val_1 = 692; //load sensor 1 (front)cal value, this value is obtained by calibrating the scale with known weight
-    int val_2 = 789; //load sensor 2 (rear) cal value, this value is obtained by calibrating the scale with known weight
-    int val_3 = 1221; //cal value 1/10mm, projected distance between wing support points
-    int val_4 = 325; //cal value 1/10mm, projected distance from front wing support point to leading edge stopper pin
-    //-----
-  */
-  /*
-    //B01 3d printed version:
-    int val_1 = 945; //load sensor 1 (front)cal value, this value is obtained by calibrating the scale with known weight
-    int val_2 = 812; //load sensor 2 (rear) cal value, this value is obtained by calibrating the scale with known weight
-    int val_3 = 1198; //cal value 1/10mm, projected distance between wing support points
-    int val_4 = 300; //cal value 1/10mm, projected distance from front wing support point to leading edge stopper pin
-    //-----
-  */
-  /*  
-    EEPROM.write(EEPROM_SENS1_ADR, val_1>>8 & 0xff); //store sensor 1 calibration, high byte
-    EEPROM.write(EEPROM_SENS1_ADR+1, val_1 & 0xff); //store sensor 1 calibration, low byte
-    EEPROM.write(EEPROM_SENS2_ADR, val_2>>8 & 0xff); //store sensor 1 calibration, high byte
-    EEPROM.write(EEPROM_SENS2_ADR+1, val_2 & 0xff); //store sensor 1 calibration, low byte
-    EEPROM.write(EEPROM_PEGDIST_ADR, val_3>>8 & 0xff); //store sensor 1 calibration, high byte
-    EEPROM.write(EEPROM_PEGDIST_ADR+1, val_3 & 0xff); //store sensor 1 calibration, low byte
-    EEPROM.write(EEPROM_STOPDIST_ADR, val_4>>8 & 0xff); //store sensor 1 calibration, high byte
-    EEPROM.write(EEPROM_STOPDIST_ADR+1, val_4 & 0xff); //store sensor 1 calibration, low byte
-  */
-  int sens_cal_1 = word(EEPROM.read(EEPROM_SENS1_ADR), EEPROM.read(EEPROM_SENS1_ADR + 1));
-  int sens_cal_2 = word(EEPROM.read(EEPROM_SENS2_ADR), EEPROM.read(EEPROM_SENS2_ADR + 1));
-  //sens_cal_2 = 812;
-  WingPegDist = word(EEPROM.read(EEPROM_PEGDIST_ADR), EEPROM.read(EEPROM_PEGDIST_ADR + 1));
-  LEstopperDist = word(EEPROM.read(EEPROM_STOPDIST_ADR), EEPROM.read(EEPROM_STOPDIST_ADR + 1));
-  CGoffset = ((WingPegDist / 2) + LEstopperDist) * 10;
-
-  scale_1.set_scale(sens_cal_1); // this value is obtained by calibrating the scale with known weights
-  scale_1.tare(); // reset the scale to 0
-  scale_2.set_scale(sens_cal_2); // this value is obtained by calibrating the scale with known weights; see the README for details //was float (942.f);
-  scale_2.tare(); // reset the scale to 0
+  delay(1000); //stabilize
+  int sens_cal_1 = 954;// this value is obtained by calibrating the front sensor with known weights
+  int sens_cal_2 = 799; // this value is obtained by calibrating the rear sensor with known weights
+  scale_1.set_scale(sens_cal_1); 
+  scale_2.tare(30); // reset the scale to 0
+  scale_2.set_scale(sens_cal_2);
+  scale_1.tare(30); // reset the scale to 0 
 
   pinMode(ledPin, OUTPUT); //led
   digitalWrite(ledPin, HIGH);
 
-  output = 1; // 0:Serial terminal, 1:LCD
+  output = 1; // 0:Serial terminal (for calibrating), 1:LCD
   int batval = readBattVoltage();
   Serial.write(254);
   Serial.write(128);
@@ -103,9 +60,9 @@ void setup() {
   Serial.write((char)((batval % 100) / 10) + 48);
   Serial.print("V     ");
   delay(3000);
-  //Serial.println();
   Serial.write(254);
   Serial.write(127); // clear lcd
+
 }
 int readBattVoltage() { // read battery voltage
   long newbattvalue = 0;
@@ -113,7 +70,7 @@ int readBattVoltage() { // read battery voltage
     newbattvalue += analogRead(batRefPin);
   }
   newbattvalue /= 5;
-  newbattvalue *= 502L; // vout=(reading * (5.00V*10000)/1023 (calculated this way in order to avoid use of floats
+  newbattvalue *= 502L; // vout=(reading * (5.00V*10000)/1023 (calculated this way in order to avoid use of floats) calibrate if required
   newbattvalue /= 32;
   //Serial.println(newbattvalue);
   if ((newbattvalue < 7500) && (battValue < 7500)) { //
@@ -131,7 +88,7 @@ void loop() {
     scale_2.power_up();
     float a = scale_1.get_units(20);
     weightAvr[0] = a * 100;
-    float b = scale_2.get_units(20);
+    float b = (scale_2.get_units(20));
     weightAvr[1] = b * 100;
     weightTot = weightAvr[0] + weightAvr[1];
     scale_1.power_down(); // put the ADC in sleep mode
@@ -153,8 +110,15 @@ void loop() {
         Serial.print("\t| average_");
         Serial.print(a + 1);
         Serial.print(":\t");
+        if(weightAvr[a] < 0) {
+          Serial.print('-');
+          weightAvr[a] = ~weightAvr[a];
+        }
         Serial.print(weightAvr[a] / 100);
         Serial.print('.');
+        if((weightAvr[a] % 100) < 10) {
+          Serial.print("0");
+        }
         Serial.println(weightAvr[a] % 100);
         Serial.print("CGratio:");
         Serial.print(CGratio);
@@ -206,7 +170,6 @@ void loop() {
         Serial.write(toLCD[i]);
       }
     }
-    
     digitalWrite(ledPin, HIGH);
     delay(100);
     digitalWrite(ledPin, LOW);
